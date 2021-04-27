e09acdbfdaff1ef7043f42901191dd7e76b07b69
Jack <jacksonmorgan@hey.com>
2021-04-26 20:30:16 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-// import {Provider} from 'react-redux'
-// import {createStore, applyMiddleware} from 'redux'
-// import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import appReducer from './appReducer'
+
+
 
 import './stylesheets/index.scss';
 import './stylesheets/reset.scss';
@@ -17,18 +20,22 @@ import HeaderWithHoles from './components/HeaderWithHoles.jsx'
 import Scorecard from './components/Scorecard.jsx'
 import HoleMenu from './components/HoleMenu.jsx'
 
+// need to look into combineReducers
+const store = createStore(appReducer, applyMiddleware(thunk))
 
-// const BASE_URL = 'http://localhost:3000'
-// const store = createStore()
+const BASE_URL = 'http://localhost:3001'
+export default BASE_URL
 
 ReactDOM.render(
-  <Router>
-    {/* commented for prod tests with netlify; need to find fix for router to be compatible */}
-    {/* <Route exact path='/' component={Header}/>
-    <Route exact path='/' component={LandingPageForm}/> */}
-    <Route path='/' component={HeaderWithHoles}/>
-    <Route path='/' component={HoleMenu}/>
-    <Route path='/' component={Scorecard}/>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      {/* commented for prod tests with netlify; need to find fix for router to be compatible */}
+      {/* <Route exact path='/' component={Header}/>
+      <Route exact path='/' component={LandingPageForm}/> */}
+      <Route path='/' component={HeaderWithHoles}/>
+      <Route path='/' component={HoleMenu}/>
+      <Route path='/' component={Scorecard}/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
